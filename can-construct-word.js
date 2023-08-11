91e32b695f0f264865f527b45d333b7d238e0950
Brianna Tran <briannatran45@gmail.com>
2023-08-11 14:59:46 -0700

    adding initial files

---
+++ can-construct-word.js
@@ -0,0 +1,11 @@
+'use strict';
+// add whatever parameters you deem necessary & write doc comment
+
+/**  */
+
+//inputs: two strings
+//outputs: boolean => T/F
+
+function canConstructWord() {
+
+}
