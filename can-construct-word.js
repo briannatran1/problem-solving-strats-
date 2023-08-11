f26d4fc413b9b6155778ab689083229639bae6d8
Brianna Tran <briannatran45@gmail.com>
2023-08-11 15:26:29 -0700

    solved can-construct-word

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,11 +1,59 @@
 'use strict';
 // add whatever parameters you deem necessary & write doc comment
 
-/**  */
+/*This should accept two strings: word and letters. It should return true if the word can be built with the letters;
+otherwise, it should return false.
+
+There are only lowercase letters (with no spaces or punctuation) in both word and letters.
+
+Constraints: time complexity: O(w + k) (if w is the length of word and k is the length of letters)*/
+
+/** See if letters string can form word string. Return boolean */
 
 //inputs: two strings
 //outputs: boolean => T/F
 
-function canConstructWord() {
+//edge cases:
+  //both strings empty?
 
+//make frequency counters for both word and letters
+//loop thorough word freq counter
+  //if values in word is greater than letters,
+    //return false
+//return true
+
+function canConstructWord(word, letters) {
+  if(letters.length === 0){
+    return false;
+  }
+
+  const wordFreq = frequencyCounter(word);
+  const lettersFreq = frequencyCounter(letters);
+
+  for(let letter in wordFreq){
+    if(!(letter in lettersFreq) || wordFreq[letter] > lettersFreq[letter]){
+      return false;
+    }
+  }
+  return true;
+}
+
+/** Making a frequency counter. Returns obj */
+
+// make frequency counter function to create counter objs for word and letters
+  // declare freq obj
+  // iterate through str using for... of loop
+    // if char exists in freq obj,
+      //increment value at key
+    //else,
+      // set equal to 1
+  //return freq obj
+
+function frequencyCounter(str){
+  const freq = {};
+  for(let char of str){
+    const curr = freq[char] || 0;
+    freq[char] = curr + 1;
+  }
+  return freq;
 }
