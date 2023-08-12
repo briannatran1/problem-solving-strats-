eede4a880f867a69b3a1624a3326a4927d09c6cb
Brianna Tran <briannatran45@gmail.com>
2023-08-11 17:01:29 -0700

    updated separate-positive

--- separate-positive.js
+++ separate-positive.js
@@ -13,37 +13,38 @@ The problem should be done in place, and the function should return the original
 //outputs: array of numbers
 
 //edge cases:
-  // all positives?
-  // all negatives?
+// all positives?
+// all negatives?
 
 //Time complexity: O(n)
 
 //two pointer method?
 //declare pointer vars => start and end
 //while start and end pointers do not overlap
-  //check numbers at start and end
-  //if both numbers at pointers needs to be swapped,
-    //swap positions
-  //else if number on start only needs to be swapped,
-    //decrement end
-  //else
-    //increment start
+//check numbers at start and end
+//if both numbers at pointers needs to be swapped,
+//swap positions
+//else if number on start only needs to be swapped,
+//decrement end
+//else
+//increment start
 //return nums
 
 function separatePositive(nums) {
   let start = 0;
   let end = nums.length - 1;
 
-  while(start < end){
-    if(nums[start] < 0 && nums[end] > 0){
+  while (start < end) {
+    if (nums[start] < 0 && nums[end] > 0) {
       [nums[start], nums[end]] = [nums[end], nums[start]];
     }
-    else if(nums[start] < 0){
+    else if (nums[start] < 0) {
       end--;
     }
-    else{
+    else {
       start++;
     }
   }
+
   return nums;
 }
