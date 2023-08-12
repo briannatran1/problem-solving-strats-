12d17ada74403f568f4b140b640c5384a692a418
Brianna Tran <briannatran45@gmail.com>
2023-08-11 17:01:16 -0700

    updated is-subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -8,11 +8,11 @@
 //declare two different pointers, one for each string that start at the beginning of each string
 
 //loop while the pointer is still going through s1
-  //if s2 is out of bounds, return false
+//if s2 is out of bounds, return false
 
-  //check if the current char in s1 is equal to the curr char in s2
-    //move BOTH pointers up by one
-  //if not, move s2's pointer up by one
+//check if the current char in s1 is equal to the curr char in s2
+//move BOTH pointers up by one
+//if not, move s2's pointer up by one
 
 // return true
 /** Determine whether s1 is a subsequence of s2 and return a boolean*/
@@ -28,7 +28,7 @@ function isSubsequence(s1, s2) {
       s1Start++;
       s2Start++;
     } else {
-      s2Start++
+      s2Start++;
     }
   }
 
